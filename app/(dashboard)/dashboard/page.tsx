6f55a9a3e68066a03277907cb8b085e643b99f20
Sridhar <sridhar.fullstack.dev.india@gmail.com>
2024-03-30 17:28:30 +0530

    Auth

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
@@ -7,6 +6,16 @@ import { auth, db, storage } from "@/lib/firebase";
 import { signOut, useSession } from "next-auth/react";
 import { getDownloadURL, ref } from "firebase/storage";
 import { User, onAuthStateChanged } from "firebase/auth";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+// Icons
+import { GoPeople } from "react-icons/go";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 
 interface UserProfileData {
   fullName: string;
@@ -52,24 +61,42 @@ export default function UserProfile() {
     <div className="h-screen overflow-y-scroll">
       {userProfile ? (
         <div>
-          <p>User ID: {userProfile.uid}</p>
-          <p>User Role: {userProfile.role}</p>
-          <h2>Name: {userProfile.fullName}</h2>
-          <p>Email: {session?.data?.user?.email}</p>
-
           {userProfile.photoURL && (
-            <Image
-              src={userProfile.photoURL}
-              alt="Profile"
-              height={512}
-              width={512}
-              className="w-12 rounded-full"
-            />
-          )}
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar>
+                  <AvatarImage src={userProfile.photoURL} alt="@shadcn" />
+                  <AvatarFallback>
+                    <GoPeople />
+                  </AvatarFallback>
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-fit text-sm">
+                <div className="flex justify-start items-center gap-4">
+                  Name: {userProfile.fullName}
+                </div>
+                <div className="flex justify-start items-center gap-4">
+                  Role: {userProfile.role}
+                </div>
+                <div className="flex justify-start items-center gap-4">
+                  Email: {session?.data?.user?.email}
+                </div>
+                <div className="flex justify-start items-center gap-4">
+                  User ID: {userProfile.uid}
+                </div>
 
-          <button className="text-black" onClick={() => signOut()}>
-            Logout
-          </button>
+                <div className="flex justify-center items-center">
+                  <Button
+                    variant="secondary"
+                    onClick={() => signOut()}
+                    className="mt-4"
+                  >
+                    Log Out
+                  </Button>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       ) : (
         <p>You need to Signin to use this page</p>
