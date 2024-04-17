70a54585805ce3bccc3bb01d22657f2070e9baa2
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-04-17 23:12:26 +0530

    Auth finished without errors

---
+++ app/(dashboard)/landing-page/page.tsx
@@ -0,0 +1,29 @@
+// Icons
+
+import { landingPageMenu } from "@/lib/constants";
+import Link from "next/link";
+
+const LandingPage = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section className="px-8">
+      <div className="flex">
+        <div className="w-[20%] text-sm py-8">
+          {landingPageMenu.map((landingPage, landingPageKey) => (
+            <Link
+              href={landingPage.href}
+              key={landingPageKey}
+              className="p-4 flex justify-between items-center"
+            >
+              {landingPage.name} <landingPage.icon />
+            </Link>
+          ))}
+        </div>
+
+        {/* Actions */}
+        <div className="w-[80%]">{children}</div>
+      </div>
+    </section>
+  );
+};
+
+export default LandingPage;
