0fe9893e730c004b5ee19315935fbbf4731d4ffd
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-04-21 21:11:07 +0530

    Commit

--- app/(dashboard)/plans/page.tsx
+++ app/(dashboard)/plans/page.tsx
@@ -13,14 +13,14 @@ const Plans: FC = () => {
         {/* Base Plan */}
         <div className="relative h-[70vh] w-full p-4 border border-green-600 text-green-600 rounded-lg shadow-md shadow-green-600 hover:scale-110 transition-all duration-500 ease-in-out">
           <div className="flex justify-center items-center gap-2">
-            <h1 className="text-center font-bold text-xs">Base Plan</h1>
+            <h1 className="text-center font-bold text-base">Base Plan</h1>
             <GiShipWheel className="text-2xl" />
           </div>
 
           <div className="w-full h-[1px] bg-green-600 my-4"></div>
 
-          <div className="px-8 my-4">
-            <ul className="list-disc">
+          <div className="px-8 my-4 text-sm">
+            <ul className="list-disc flex flex-col justify-center gap-2">
               <li>Admin Access</li>
               <li>Access Dashboard</li>
               <li>Access Business</li>
@@ -50,16 +50,16 @@ const Plans: FC = () => {
         </div>
 
         {/* Base Plan + Pro */}
-        <div className="h-[70vh] w-full p-4 border border-blue-600 text-blue-600 rounded-lg shadow-md shadow-blue-600 hover:scale-110 transition-all duration-500 ease-in-out">
+        <div className="relative h-[70vh] w-full p-4 border border-blue-600 text-blue-600 rounded-lg shadow-md shadow-blue-600 hover:scale-110 transition-all duration-500 ease-in-out">
           <div className="flex justify-center items-center gap-2">
-            <h1 className="text-center font-bold text-xs">BP + Pro</h1>
+            <h1 className="text-center font-bold text-base">BP + Pro</h1>
             <LuSailboat className="text-lg" />
           </div>
 
           <div className="w-full h-[1px] bg-blue-600 my-4"></div>
 
-          <div className="px-8 my-4">
-            <ul className="list-disc">
+          <div className="px-8 my-4 text-sm">
+            <ul className="list-disc flex flex-col justify-center gap-2">
               <li>Admin Access</li>
               <li>Access Dashboard</li>
               <li>Access Business</li>
@@ -72,19 +72,35 @@ const Plans: FC = () => {
               <li>Maximum 1TB Of Storage</li>
             </ul>
           </div>
+
+          <div className="absolute bottom-10 left-10">
+            <Button
+              className="bg-blue-600 border-blue-600 text-white hover:text-blue-600 hover:bg-white"
+              variant={"outline"}
+            >
+              Edit
+            </Button>
+
+            <Button
+              className="ml-4 bg-blue-600 border-blue-600 text-white hover:text-blue-600 hover:bg-white"
+              variant={"outline"}
+            >
+              See Requests
+            </Button>
+          </div>
         </div>
 
         {/* Base Plan + Pro + Premium */}
-        <div className="h-[70vh] w-full p-4 border border-yellow-600 text-yellow-600 rounded-lg shadow-md shadow-yellow-600 hover:scale-110 transition-all duration-500 ease-in-out">
+        <div className="relative h-[70vh] w-full p-4 border border-yellow-600 text-yellow-600 rounded-lg shadow-md shadow-yellow-600 hover:scale-110 transition-all duration-500 ease-in-out">
           <div className="flex justify-center items-center gap-2">
-            <h1 className="text-center font-bold text-xs">BP + Pro + Premium</h1>
+            <h1 className="text-center font-bold text-base">BP + Pro + Premium</h1>
             <LuCrown className="text-lg" />
           </div>
 
           <div className="w-full h-[1px] bg-yellow-600 my-4"></div>
 
-          <div className="px-8 my-4">
-            <ul className="list-disc">
+          <div className="px-8 my-4 text-sm">
+            <ul className="list-disc flex flex-col justify-center gap-2">
               <li>Admin Access</li>
               <li>Access Dashboard</li>
               <li>Access Business</li>
@@ -96,6 +112,22 @@ const Plans: FC = () => {
               <li>Customizable Settings</li>
               <li>Maximum 1TB Of Storage</li>
             </ul>
+          </div>
+
+          <div className="absolute bottom-10 left-10">
+            <Button
+              className="bg-yellow-600 border-yellow-600 text-white hover:text-yellow-600 hover:bg-white"
+              variant={"outline"}
+            >
+              Edit
+            </Button>
+
+            <Button
+              className="ml-4 bg-yellow-600 border-yellow-600 text-white hover:text-yellow-600 hover:bg-white"
+              variant={"outline"}
+            >
+              See Requests
+            </Button>
           </div>
         </div>
       </div>
